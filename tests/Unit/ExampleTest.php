7223a886514662ca72832d292612fd6604cc32cf
g <gharnett@gmail.com>
2017-02-15 08:36:55 -0500

    unit tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -5,7 +5,10 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-
+use App\User;
+use App\Question;
+use App\Answer;
+use App\Survey;
 class ExampleTest extends TestCase
 {
     /**
@@ -15,6 +18,30 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        // create user in DB
+        $user = factory(User::class)->create();
+
+        // create question in DB
+        $question = factory(Question::class)->create();
+
+        // create answer in DB
+        $answer = factory(Answer::class)->create();
+
+        // create survey in DB
+        $answer = factory(Survey::class)->create();
+
+        /* test access to questions admin section for authenticated user */
+        $response = $this->actingAs($user)->get('admin/questions');
+        $response->assertStatus(200);
+
+        /* test access to answers admin section for authenticated user */
+        $response = $this->actingAs($user)->get('admin/answers');
+        $response->assertStatus(200);
+
+        /* test access to surveys admin section for authenticated user */
+        $response = $this->actingAs($user)->get('admin/surveys');
+        $response->assertStatus(200);
+
+
     }
 }
